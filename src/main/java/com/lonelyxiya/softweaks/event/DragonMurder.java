f73324e2b714f8f7289ec5db67a40dfb86fcd084
Lonelyxiya <3438505487@qq.com>
2024-02-12 22:58:36 +0800

    fix Dragon bug

--- src/main/java/com/lonelyxiya/softweaks/event/DragonMurder.java
+++ src/main/java/com/lonelyxiya/softweaks/event/DragonMurder.java
@@ -3,20 +3,14 @@ package com.lonelyxiya.softweaks.event;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.boss.EntityDragon;
-import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProviderEnd;
-import net.minecraft.world.biome.BiomeEndDecorator;
 import net.minecraft.world.end.DragonFightManager;
-import net.minecraft.world.gen.feature.WorldGenSpikes;
-
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 
 public class DragonMurder {
 
@@ -24,7 +18,6 @@ public class DragonMurder {
     public void preInit(FMLPreInitializationEvent e) {
         MinecraftForge.EVENT_BUS.register(this);
     }
-
 
     @SubscribeEvent
     public void join(EntityJoinWorldEvent e) {
@@ -36,16 +29,10 @@ public class DragonMurder {
             DragonFightManager dragonFightManager = endProvider.getDragonFightManager();
 
             if (entity instanceof EntityDragon && dragonFightManager != null && !dragonFightManager.hasPreviouslyKilledDragon()) {
-
+                
                 EntityDragon dragon = new EntityDragon(world);
-                dragon.setLocationAndAngles(entity.posX, entity.posY, entity.posZ, world.rand.nextFloat() * 360.0F, 0.0F);
+                dragon.setPosition(entity.posX, entity.posY, entity.posZ);
                 world.spawnEntity(dragon);
-
-
-                for (WorldGenSpikes.EndSpike spike : BiomeEndDecorator.getSpikesForWorld(world)) {
-                    world.getEntitiesWithinAABB(EntityEnderCrystal.class, spike.getTopBoundingBox())
-                            .forEach(crystal -> crystal.setDead());
-                }
             }
         }
     }

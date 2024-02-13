2f79b4debf1fdca6c1c299db399000432260be5f
Lonelyxiya <3438505487@qq.com>
2024-02-13 12:33:57 +0800

    fix bug4

--- src/main/java/com/lonelyxiya/softweaks/event/DragonMurder.java
+++ src/main/java/com/lonelyxiya/softweaks/event/DragonMurder.java
@@ -3,28 +3,44 @@ package com.lonelyxiya.softweaks.event;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldProviderEnd;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+
+@Mod.EventBusSubscriber()
 public class DragonMurder {
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent e) {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public void preInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void join(EntityJoinWorldEvent e) {
-        Entity entity = e.getEntity();
-        World world = entity.getEntityWorld();
+    public static void dragonmurder(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        World world = event.getWorld();
 
-        if (!world.isRemote && world.provider instanceof WorldProviderEnd && entity instanceof EntityDragon) {
-            entity.setDead();
+        if (!world.isRemote && world.provider.getDimension() == 1) {
+
+            if (entity instanceof EntityDragon) {
+                List<EntityEnderCrystal> list = world.getEntitiesWithinAABB(EntityEnderCrystal.class, entity.getEntityBoundingBox().grow(320.0D));
+                for (EntityEnderCrystal enderCrystal : list) {
+                    enderCrystal.setDead();
+                }
+
+                ((EntityDragon) entity).setHealth(0.0F);
+                LOGGER.debug("Good night Ender Dragon.");
+            }
         }
     }
 }

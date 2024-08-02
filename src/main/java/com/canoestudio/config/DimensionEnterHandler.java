f56fb6b20c3c5010477bd57ee0357aab6fc7eac8
Lonelyxiya <3438505487@qq.com>
2024-08-02 19:10:36 +0800

    b

--- src/main/java/com/canoestudio/config/DimensionEnterHandler.java
+++ src/main/java/com/canoestudio/config/DimensionEnterHandler.java
@@ -5,19 +5,24 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class DimensionEnterHandler {
 
-    private static String teleportMessage = "You did not hold the required item and have been teleported to your spawn point.";
+    private static String preventBreakMessage = "You did not hold the required item and cannot break blocks in the Nether.";
     private static Map<Integer, ItemRequirement> dimensionItemRequirements = new HashMap<>();
+    private static Set<String> playersWithoutItem = new HashSet<>();
 
     public static void initConfig(File configDir) {
         File configFile = new File(configDir, "dimension_enter_config.txt");
@@ -29,7 +34,6 @@ public class DimensionEnterHandler {
 
                 // 添加默认配置
                 config.get(Configuration.CATEGORY_GENERAL, "-1:minecraft:diamond_sword:0", "required");
-                config.get(Configuration.CATEGORY_GENERAL, "0:minecraft:golden_apple:1", "required");
 
                 if (config.hasChanged()) {
                     config.save();
@@ -54,24 +58,33 @@ public class DimensionEnterHandler {
     }
 
     @SubscribeEvent
-    public void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
-        EntityPlayer player = event.player;
-        int dimension = event.toDim;
+    public void onEntityTravelToDimension(EntityTravelToDimensionEvent event) {
+        if (event.getEntity() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.getEntity();
+            int dimension = event.getDimension();
 
-        if (dimensionItemRequirements.containsKey(dimension)) {
-            ItemRequirement requirement = dimensionItemRequirements.get(dimension);
-            ItemStack heldItem = player.getHeldItemMainhand();
+            if (dimensionItemRequirements.containsKey(dimension)) {
+                ItemRequirement requirement = dimensionItemRequirements.get(dimension);
+                ItemStack heldItem = player.getHeldItemMainhand();
 
-            boolean hasRequiredItem = heldItem.getItem() == ForgeRegistries.ITEMS.getValue(requirement.item) && heldItem.getMetadata() == requirement.meta;
+                boolean hasRequiredItem = heldItem.getItem() == ForgeRegistries.ITEMS.getValue(requirement.item) && heldItem.getMetadata() == requirement.meta;
 
-            if (hasRequiredItem) {
-                heldItem.shrink(1); // 消耗一个物品
-            } else {
-                if (dimension == -1) { // 特殊处理进入地狱
-                    player.setPositionAndUpdate(player.getBedLocation().getX(), player.getBedLocation().getY(), player.getBedLocation().getZ());
-                    player.sendMessage(new TextComponentTranslation(teleportMessage));
+                if (dimension == -1 && !hasRequiredItem) {
+                    playersWithoutItem.add(player.getUniqueID().toString());
+                    player.sendMessage(new TextComponentTranslation(preventBreakMessage));
+                } else {
+                    playersWithoutItem.remove(player.getUniqueID().toString());
                 }
             }
+        }
+    }
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        EntityPlayer player = event.getPlayer();
+        if (player.world.provider.getDimension() == -1 && playersWithoutItem.contains(player.getUniqueID().toString())) {
+            event.setCanceled(true);
+            player.sendMessage(new TextComponentTranslation(preventBreakMessage));
         }
     }
 

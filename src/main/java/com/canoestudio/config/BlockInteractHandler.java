f1b4201022f37330d68eb9795ea7b3da590e4373
Lonelyxiya <3438505487@qq.com>
2024-08-11 11:01:25 +0800

    fixed bug fuck

--- src/main/java/com/canoestudio/config/BlockInteractHandler.java
+++ src/main/java/com/canoestudio/config/BlockInteractHandler.java
@@ -2,12 +2,11 @@ package com.canoestudio.config;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -21,19 +20,6 @@ import java.util.Set;
 public class BlockInteractHandler {
 
     private static Set<BlockData> interactBlocks = new HashSet<>();
-    private boolean isIntegratedServer;
-    private boolean isDedicatedServer;
-
-    public BlockInteractHandler() {
-        MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
-        if (server != null) {
-            isIntegratedServer = server.isSinglePlayer();
-            isDedicatedServer = server.isDedicatedServer();
-        } else {
-            isIntegratedServer = false;
-            isDedicatedServer = false;
-        }
-    }
 
     public static void initConfig(File configDir) {
         File configFile = new File(configDir, "block_interact_config.txt");
@@ -49,7 +35,6 @@ public class BlockInteractHandler {
 
         config.load();
 
-        // 从配置文件加载需要处理的方块列表
         String[] defaultInteractBlocks = {
                 "minecraft:crafting_table:0=5",
                 "minecraft:furnace:0=5",
@@ -66,11 +51,9 @@ public class BlockInteractHandler {
     public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
         if (event.getWorld().isRemote) return;
 
-        // 集成服务器和独立服务器的判断
-        if (isIntegratedServer) {
-            // 集成服务器的逻辑
-        } else if (isDedicatedServer) {
-            // 独立服务器的逻辑
+        // 仅处理主手交互
+        if (event.getHand() != EnumHand.MAIN_HAND) {
+            return;
         }
 
         EntityPlayer player = event.getEntityPlayer();
@@ -78,13 +61,14 @@ public class BlockInteractHandler {
         int meta = event.getWorld().getBlockState(event.getPos()).getBlock().getMetaFromState(event.getWorld().getBlockState(event.getPos()));
 
         if (isInteractableBlock(block, meta)) {
+            // 检查玩家是否有足够的经验等级
             int requiredExperienceLevels = getRequiredExperienceLevels(block, meta);
             if (player.experienceLevel >= requiredExperienceLevels) {
                 player.addExperienceLevel(-requiredExperienceLevels);
                 player.sendMessage(new TextComponentString(I18n.format("message.block_interact_success", block.getRegistryName().toString(), requiredExperienceLevels)));
             } else {
                 player.sendMessage(new TextComponentString(I18n.format("message.not_enough_experience", requiredExperienceLevels)));
-                event.setCanceled(true);
+                event.setCanceled(true); // 阻止交互，防止打开GUI
             }
         }
     }
